34509a53dea0932313f869a7a3179856e15ee8bf
Kirill Prokhorenko <k_prokhorenko@wargaming.net>
2017-12-11 13:15:25 +0300

    local curve crutch

--- node.go
+++ node.go
@@ -57,7 +57,11 @@ func (this *Node) PropDate(name string, defaultValue time.Time) time.Time {
 		}
 		output, err = time.ParseInLocation("20060102T150405", node.Value, loc)
 	} else {
-		output, err = time.Parse("20060102T150405", node.Value)
+		loc, err := time.LoadLocation("Europe/Moscow")
+		if err != nil {
+			panic(err)
+		}
+		output, err = time.ParseInLocation("20060102T150405", node.Value, loc)
 	}
 
 	if err != nil {
